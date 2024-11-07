7db0a7d4a04600b9481e10b42cc5a6db4bbabad8
hwabrahamson <henryabrahamson2022@u.northwestern.edu>
2024-11-07 13:01:47 -0600

    Update waggle_scheduler.py

--- scheduler/waggle_scheduler.py
+++ scheduler/waggle_scheduler.py
@@ -5,6 +5,22 @@ from copy import deepcopy
 
 
 class WaggleScheduler(Scheduler):
+    
+    # Objective function to be minimized (probably eventually move this to the yaml)
+    # policy indicates the decision variable, written as an N by M array, where
+    # policy[i,t] is the number of jobs of type t allocated to node i
+    #
+    # alpha is a scalar between 0 & 1, a tunable parameter for alpha fairness.
+    # 
+    # power is the vector of powers needed for each job (power[t] is the power for that task)
+    # 
+    # state indicates the current job allocation right before schedulling as an N by M array,
+    # i.e., state[i,t] is the power allocated to task(s) t at node i. In terms of the writeup,
+    # state[i,t] at time k = x_i^t(k-1) - O_i^t(k)
+    def objective(policy,alpha,power,state)
+        X = sum(policy*power + state, axis=0)
+        return sum(X^(1-alpha)/(1-alpha))
+    
     def __init__(self):
         super().__init__()
 
